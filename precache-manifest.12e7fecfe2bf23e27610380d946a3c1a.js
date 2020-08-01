c9b0c242bf3ed8edc48325f3c56fe1fe61e5ffb3
faiz ahamed <faizpahamed@gmail.com>
2020-08-01 15:01:54 +0530

    Updates

---
+++ precache-manifest.12e7fecfe2bf23e27610380d946a3c1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa4e028ee475c206cff36df07b049c2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8427056401f949a9c3aa",
+    "url": "/monsters-rolodex/static/css/main.a507e83d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8427056401f949a9c3aa",
+    "url": "/monsters-rolodex/static/js/main.4af919ab.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
